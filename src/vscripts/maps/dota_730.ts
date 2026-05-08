d00c7dca7a76f54f041017392fcd745303ebd50d
ThLion228 <egorvyblov24@gmail.com>
2026-05-08 09:25:06 +0500

    Add visual console commands to 730 map settings

--- src/vscripts/maps/dota_730.ts
+++ src/vscripts/maps/dota_730.ts
@@ -5,6 +5,15 @@ export class Dota730Map extends MapSettings {
         holder.disable(BuiltInModifier.FOUNTAIN_INVULNERABILITY);
     }
 
+    public SendToConsole(): void {
+        SendToServerConsole("dota_alt_right_range_hint 0");
+        SendToServerConsole("dota_alt_show_rune_spawn_times 0");
+        SendToServerConsole("dota_alt_show_ward_suggestions 0");
+        SendToServerConsole("dota_alt_show_neutral_stack_times_and_arrow 0");
+        SendToServerConsole("dota_alt_show_neutral_creep_gold_bounty 0");
+        SendToServerConsole("dota_alt_show_lane_creep_gold_bounty 0");
+    }
+
     public Configure(): void {
         GameModeEntity.SetAllowNeutralItemDrops(false);
     }
